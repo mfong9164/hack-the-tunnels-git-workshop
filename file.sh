0e1a9dcc132a4a757ac28ce962013e807de91396
Michael <mfong9164@gmail.com>
2024-10-18 20:35:03 -0400

    initial commit

---
+++ file.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Hack the Tunnels"
+cowsay "Hack the Tunnels" | lolcat
+cowsay "Hack the Tunnels" | lolcat
+cowsay "Hack the Tunnels" | lolcat
+cowsay "Hack the Tunnels" | lolcat
+cowsay "Hack the Tunnels" | lolcat
+cowsay "Hack the Tunnels" | lolcat
